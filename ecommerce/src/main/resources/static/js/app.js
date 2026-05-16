0a022a6fe4c0a9901824abeb9d8f8729ac2e0bd7
Andres Perez De La Vega <aperezd@ilunion.com>
2026-05-16 17:40:09 +0200

    E-commerce: Estructura con catálogo y filtrar por categoria, nombre, dinero , carrito mejorado ,valoraciones, registro de compra en el perfil, graficas en el admin dashboard para mejorar el seguimiento

--- ecommerce/src/main/resources/static/js/app.js
+++ ecommerce/src/main/resources/static/js/app.js
@@ -221,9 +221,14 @@ function verificarSesion() {
 
         container.innerHTML = `
             ${btnAdmin}
-            <span onclick="location.href='perfil.html'" style="cursor:pointer; text-decoration:underline; font-weight:bold; margin-right:15px;">
-                👤 ${usuario.username}
+
+            <span onclick="location.href='perfil.html'" style="cursor:pointer; color: white; font-weight:bold; margin-right:15px; display: inline-flex; align-items: center; text-decoration: none;">
+                <svg width="22" height="22" fill="currentColor" viewBox="0 0 24 24" style="margin-right: 6px;">
+                    <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
+                </svg>
+                ${usuario.username}
             </span>
+
             <button onclick="cerrarSesion()" style="background-color: #95a5a6; margin-right: 10px;">Salir</button>
             <button id="btn-carrito" onclick="location.href='carrito.html'">
                 🛒 Carrito (<span id="cart-count">0</span>)
